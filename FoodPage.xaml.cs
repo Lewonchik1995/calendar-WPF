0226b5d59cb1c27e810a3dd191499e3a26180b3f
Lewonchik1995 <t50_a.s.protsenko@mpt.ru>
2023-05-18 16:01:00 +0300

    Практическая работа 5

---
+++ FoodPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace calendar
+{
+    public partial class FoodPage : Page
+    {
+        public FoodPage()
+        {
+            InitializeComponent();
+
+            FoodUserControl breakfast = new FoodUserControl();
+            breakfast.CheckBoxFood.IsChecked = false;
+            breakfast.ImageFood.Source = new BitmapImage(new Uri("images\\breakfast.png", UriKind.Relative));
+            breakfast.NameFood.Text = "Завтрак";
+
+            FoodUserControl lunch = new FoodUserControl();
+            breakfast.CheckBoxFood.IsChecked = false;
+            lunch.ImageFood.Source = new BitmapImage(new Uri("images\\lunch.png", UriKind.Relative));
+            lunch.NameFood.Text = "Обед";
+
+            FoodUserControl snack = new FoodUserControl();
+            breakfast.CheckBoxFood.IsChecked = false;
+            snack.ImageFood.Source = new BitmapImage(new Uri("images\\snack.png", UriKind.Relative));
+            snack.NameFood.Text = "Перекус";
+
+            FoodUserControl dinner = new FoodUserControl();
+            breakfast.CheckBoxFood.IsChecked = false;
+            dinner.ImageFood.Source = new BitmapImage(new Uri("images\\dinner.png", UriKind.Relative));
+            dinner.NameFood.Text = "Ужин";
+
+            List<FoodUserControl> food_list = new List<FoodUserControl>() { breakfast, lunch, snack, dinner };
+            FoodList.ItemsSource = food_list;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Food.Content = new MainPage();
+        }
+
+        private void SandE_Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace calendar
+{
+    public partial class MainPage : Page
+    {
+        public static Dictionary <DateTime, List<FoodUserControl>> DFood = new Dictionary <DateTime, List<FoodUserControl>>();
+        public MainPage()
+        {
+            InitializeComponent();
+            Calendar.SelectedDate = DateTime.Now;
+            Panel();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Calendar.SelectedDate = ((DateTime)Calendar.SelectedDate).AddMonths(+1);
+            Panel();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Calendar.SelectedDate = ((DateTime)Calendar.SelectedDate).AddMonths(-1);
+            Panel();
+        }
+
+        public void Panel()
+        {
+            Day.Children.Clear();
+
+            for (int i = 1; i <= DateTime.DaysInMonth(((DateTime)Calendar.SelectedDate).Year, ((DateTime)Calendar.SelectedDate).Month); i++)
+            {
+                DayUserControl day = new DayUserControl();
+                day.Number.Content = i.ToString();
+                day.Image.Source = new BitmapImage(new Uri("images\\dots.png", UriKind.Relative));
+                day.DayNum.Click += DayName_Click;
+                Day.Children.Add(day);
+            }
+        }
+
+        private void DayName_Click(object sender, RoutedEventArgs e)
+        {
+            FrameCheck.Content = new FoodPage();
+        }
+    }
+}
